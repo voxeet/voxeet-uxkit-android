32f857458ca4015825bb45c4e85176c94642da53
codlab <codlabtech@gmail.com>
2018-03-20 11:25:23 +0100

    add pre-cached user when joining

--- toolkit/src/main/java/sdk/voxeet/com/toolkit/controllers/ConferenceToolkitController.java
+++ toolkit/src/main/java/sdk/voxeet/com/toolkit/controllers/ConferenceToolkitController.java
@@ -1,8 +1,15 @@
 package sdk.voxeet.com.toolkit.controllers;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import org.greenrobot.eventbus.EventBus;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import sdk.voxeet.com.toolkit.main.VoxeetToolkit;
 import sdk.voxeet.com.toolkit.providers.containers.DefaultConferenceProvider;
@@ -10,6 +17,7 @@ import sdk.voxeet.com.toolkit.providers.logics.DefaultConferenceSubViewProvider;
 import sdk.voxeet.com.toolkit.views.uitookit.sdk.overlays.OverlayState;
 import sdk.voxeet.com.toolkit.views.uitookit.sdk.overlays.abs.IExpandableViewProviderListener;
 import voxeet.com.sdk.core.VoxeetSdk;
+import voxeet.com.sdk.json.UserInfo;
 
 /**
  * Created by kevinleperf on 15/01/2018.
@@ -17,8 +25,12 @@ import voxeet.com.sdk.core.VoxeetSdk;
 
 public class ConferenceToolkitController extends AbstractConferenceToolkitController implements IExpandableViewProviderListener {
 
+    private Map<String, UserInfo> mCachedInvited;
+
     public ConferenceToolkitController(Context context, EventBus eventbus, OverlayState overlay) {
         super(context, eventbus);
+
+        mCachedInvited = new HashMap<>();
 
         setDefaultOverlayState(overlay);
         setVoxeetOverlayViewProvider(new DefaultConferenceProvider(this));
@@ -35,11 +47,38 @@ public class ConferenceToolkitController extends AbstractConferenceToolkitContro
         //nothing to do
     }
 
-    public void join(String conference_id) {
+    public void join(@NonNull String conference_id, @Nullable UserInfo from_invitation) {
+        mCachedInvited.clear();
+        if(null != from_invitation) {
+            mCachedInvited.put(from_invitation.getExternalId(), from_invitation);
+        }
+
         VoxeetToolkit.getInstance().getReplayMessageToolkit().enable(false);
         enable(true);
 
         VoxeetSdk.getInstance().getConferenceService().join(conference_id);
+    }
+
+    public void join(@NonNull String conference_id) {
+        join(conference_id, null);
+    }
+
+    public void invite(@NonNull List<UserInfo> to_invite) {
+        List<String> to_list = new ArrayList<>();
+
+        for (UserInfo infos : to_invite) {
+            if(null != infos) {
+                to_list.add(infos.getExternalId());
+                mCachedInvited.put(infos.getExternalId(), infos);
+            }
+        }
+
+        VoxeetSdk.getInstance().getConferenceService().invite(null, to_list);
+    }
+
+    @Nullable
+    public UserInfo getInvitedUserFromCache(@NonNull String externalId) {
+        return (null != mCachedInvited) ? mCachedInvited.get(externalId) : null;
     }
 
     public void demo() {
