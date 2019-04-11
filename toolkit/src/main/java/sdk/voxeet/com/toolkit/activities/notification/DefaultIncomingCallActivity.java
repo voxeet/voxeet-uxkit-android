87be584fdfc147f64b77ac1a3fb6b5bf56923939
codlab <codlabtech@gmail.com>
2019-04-11 21:06:56 +0200

    remove deprecated

--- toolkit/src/main/java/sdk/voxeet/com/toolkit/activities/notification/DefaultIncomingCallActivity.java
+++
@@ -1,6 +0,0 @@
-package sdk.voxeet.com.toolkit.activities.notification;
-
-@Deprecated
-public class DefaultIncomingCallActivity extends com.voxeet.toolkit.activities.notification.DefaultIncomingCallActivity {
-
-}
--- toolkit/src/main/java/sdk/voxeet/com/toolkit/activities/workflow/VoxeetAppCompatActivity.java
+++
@@ -1,8 +0,0 @@
-package sdk.voxeet.com.toolkit.activities.workflow;
-
-/**
- * Created by kevinleperf on 21/10/2018.
- */
-@Deprecated
-public class VoxeetAppCompatActivity extends com.voxeet.toolkit.activities.VoxeetAppCompatActivity {
-}
