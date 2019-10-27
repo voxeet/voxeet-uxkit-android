ef01bcbe6c8f3cf2e1af10a9fc6d30fc6e554de2
codlab <codlabtech@gmail.com>
2019-10-27 23:48:57 +0100

    use proper push version and new application instanciation

--- app/src/main/java/fr/voxeet/sdk/sample/application/SampleApplication.java
+++ app/src/main/java/fr/voxeet/sdk/sample/application/SampleApplication.java
@@ -5,23 +5,25 @@ import android.support.annotation.Nullable;
 import android.support.multidex.MultiDexApplication;
 import android.util.Log;
 
+import com.voxeet.push.center.NotificationCenterFactory;
+import com.voxeet.push.center.management.EnforcedNotificationMode;
+import com.voxeet.push.center.management.NotificationMode;
+import com.voxeet.push.center.management.VersionFilter;
 import com.voxeet.push.firebase.FirebaseController;
+import com.voxeet.push_manifest.BuildConfig;
 import com.voxeet.sdk.core.VoxeetSdk;
-import com.voxeet.sdk.core.preferences.VoxeetPreferences;
-import com.voxeet.sdk.factories.VoxeetIntentFactory;
 import com.voxeet.sdk.json.UserInfo;
-import com.voxeet.sdk.sample.BuildConfig;
 import com.voxeet.toolkit.activities.notification.DefaultIncomingCallActivity;
 import com.voxeet.toolkit.controllers.ConferenceToolkitController;
 import com.voxeet.toolkit.controllers.VoxeetToolkit;
 import com.voxeet.toolkit.implementation.overlays.OverlayState;
+import com.voxeet.toolkit.incoming.IncomingFullScreen;
+import com.voxeet.toolkit.incoming.IncomingNotification;
 
 import org.greenrobot.eventbus.EventBus;
 
-import eu.codlab.simplepromise.Promise;
 import eu.codlab.simplepromise.solve.ErrorPromise;
 import eu.codlab.simplepromise.solve.PromiseExec;
-import eu.codlab.simplepromise.solve.PromiseSolver;
 import eu.codlab.simplepromise.solve.Solver;
 
 public class SampleApplication extends MultiDexApplication {
@@ -30,15 +32,10 @@ public class SampleApplication extends MultiDexApplication {
     private static final String TAG = SampleApplication.class.getSimpleName();
 
     private UserInfo _current_user;
-    private boolean sdkInitialized;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        Log.d(TAG, "onCreate: starting Voxeet Sample");
-
-        sdkInitialized = false;
 
         VoxeetToolkit.initialize(this, EventBus.getDefault())
                 .enableOverlay(true);
@@ -46,23 +43,17 @@ public class SampleApplication extends MultiDexApplication {
         FirebaseController.getInstance().enable(true);
 
         //change the overlay used by default
-        VoxeetToolkit.getInstance().getConferenceToolkit().setScreenShareEnabled(true)
+        VoxeetToolkit.instance().getConferenceToolkit().setScreenShareEnabled(true)
                 .setDefaultOverlayState(OverlayState.EXPANDED);
 
         VoxeetToolkit.instance().enable(ConferenceToolkitController.class);
 
         //the default case of this SDK is to have the SDK with consumerKey and consumerSecret embedded
-        uniqueInitializeSDK().then(new PromiseExec<Boolean, Object>() {
-            @Override
-            public void onCall(@Nullable Boolean result, @NonNull Solver<Object> solver) {
-                Log.d(TAG, "onCall: SDK initialized using keys");
-            }
-        }).error(new ErrorPromise() {
-            @Override
-            public void onError(@NonNull Throwable error) {
-                error.printStackTrace();
-            }
-        });
+        VoxeetSdk.initialize(
+                BuildConfig.CONSUMER_KEY,
+                BuildConfig.CONSUMER_SECRET
+        ); //can be null - will be removed in a later version
+        onSdkInitialized();
     }
 
     //when enabling multidex in your app
@@ -129,35 +120,15 @@ public class SampleApplication extends MultiDexApplication {
         return _current_user;
     }
 
-
-    /**
-     * For the example, this method will return a promise compatible with the two different type of usage
-     * of this SDK
-     *
-     * @return a promise in case of the OAuth use case, null otherwise
-     */
-    @NonNull
-    public Promise<Boolean> uniqueInitializeSDK() {
-        return new Promise<>(new PromiseSolver<Boolean>() {
-            @Override
-            public void onCall(@NonNull Solver<Boolean> solver) {
-                VoxeetSdk.initialize(
-                        BuildConfig.CONSUMER_KEY,
-                        BuildConfig.CONSUMER_SECRET); //can be null - will be removed in a later version
-
-                onSdkInitialized();
-                solver.resolve(true);
-            }
-        });
-    }
-
     private void onSdkInitialized() {
         VoxeetSdk.conference().setTimeOut(ONE_MINUTE);
 
         //it's possible to use the meta-data in the AndroidManifest to directly control the default incoming activity
-        VoxeetPreferences.setDefaultActivity(DefaultIncomingCallActivity.class.getCanonicalName());
-        VoxeetSdk.instance().register(this);
+        NotificationCenterFactory.instance.register(NotificationMode.FULLSCREEN_INCOMING_CALL, new IncomingFullScreen(DefaultIncomingCallActivity.class));
+        NotificationCenterFactory.instance.register(NotificationMode.OVERHEAD_INCOMING_CALL, new IncomingNotification());
+        NotificationCenterFactory.instance.setEnforcedNotificationMode(EnforcedNotificationMode.MIXED_INCOMING_CALL);
 
-        sdkInitialized = true;
+        //add filter to excluse fullscreen from Android Q
+        NotificationCenterFactory.instance.register(NotificationMode.FULLSCREEN_INCOMING_CALL, new VersionFilter(VersionFilter.ALL, 29));
     }
 }
