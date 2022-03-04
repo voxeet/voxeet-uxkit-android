2282f8fc4fea41040336b0ed83af15db116ccc58
Kevin Le Perf <kevin.leperf@dolby.com>
2022-03-04 04:01:50 +0100

    Filter permissions which need to be asked

--- uxkit-common/src/main/java/com/voxeet/uxkit/common/permissions/PermissionManager.java
+++ uxkit-common/src/main/java/com/voxeet/uxkit/common/permissions/PermissionManager.java
@@ -3,6 +3,9 @@ package com.voxeet.uxkit.common.permissions;
 import androidx.annotation.NonNull;
 
 import com.voxeet.promise.Promise;
+import com.voxeet.sdk.utils.Map;
+import com.voxeet.sdk.utils.MapCallback;
+import com.voxeet.sdk.utils.MapFilter;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
@@ -19,6 +22,15 @@ public class PermissionManager {
 
     public void register(@NonNull IRequestPermissions callback) {
         this.callback = new WeakReference<>(callback);
+    }
+
+    public boolean hasPermission(@NonNull String permission) throws Exception {
+        IRequestPermissions callback = this.callback.get();
+
+        if (null == callback) {
+            throw new Exception("unable to call hasPermission: invalid permission callback");
+        }
+        return callback.hasPermission(permission);
     }
 
     public boolean isPermissionNeverAskAgain(@NonNull String permission) throws Exception {
@@ -49,14 +61,28 @@ public class PermissionManager {
                 return;
             }
 
+            List<String> granted_permissions = Map.filter(permissions, callback::hasPermission);
+            List<String> ungranted_permissions = Map.filter(permissions, s -> !callback.hasPermission(s));
+
             if (permissions.size() == 0) {
                 solver.resolve(new ArrayList<>());
                 return;
             }
 
-            callback.requestPermissions(permissions, update -> {
+            List<PermissionResult> granted = Map.map(granted_permissions, p -> new PermissionResult(p, true));
+
+            if(ungranted_permissions.size() == 0) {
+                solver.resolve(granted);
+                return;
+            }
+
+            callback.requestPermissions(ungranted_permissions, update -> {
                 List<PermissionResult> output = com.voxeet.sdk.utils.Map.map(update.keySet(),
                         perm -> new PermissionResult(perm, update.get(perm)));
+
+                if(granted.size() > 0) {
+                    output.addAll(granted);
+                }
 
                 solver.resolve(output);
             });
