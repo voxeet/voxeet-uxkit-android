ddf4a6f0a5037f2dca93348b788df3903c4f6f12
Kévin Le Perf <codlabtech@gmail.com>
2023-02-28 07:29:46 +0100

    Properly register the UXKit

---
+++ uxkit/src/main/java/com/voxeet/uxkit/manifests/UXKitManifestComponent.java
@@ -0,0 +1,33 @@
+package com.voxeet.uxkit.manifests;
+
+import android.content.Context;
+import android.content.pm.ProviderInfo;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.voxeet.VoxeetSDK;
+import com.voxeet.sdk.manifests.AbstractManifestComponentProvider;
+import com.voxeet.uxkit.BuildConfig;
+import com.voxeet.uxkit.common.UXKitLogger;
+import com.voxeet.uxkit.common.logging.ShortLogger;
+
+public final class UXKitManifestComponent extends AbstractManifestComponentProvider {
+
+    private final static ShortLogger Log = UXKitLogger.createLogger(UXKitManifestComponent.class);
+
+    @Override
+    protected void init(@NonNull Context context, @Nullable ProviderInfo providerInfo) {
+        VoxeetSDK.registerComponentVersion("android-uxkit", BuildConfig.VERSION_NAME);
+    }
+
+    @Override
+    protected String getComponentName() {
+        return UXKitManifestComponent.class.getSimpleName();
+    }
+
+    @Override
+    protected String getDefaultAuthority() {
+        return "com.voxeet.uxkit.manifests.";
+    }
+}
